543f0b52c4ced89981914cba8d4f8ab704d8e35e
gustavo <gustavo.isaac@gmail.com>
2025-03-19 17:50:29 +0000

    Colors and background image

--- app/src/main/java/com/artemissoftware/demeterhub/ui/theme/Color.kt
+++ app/src/main/java/com/artemissoftware/demeterhub/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Primary = Color(0xFFFE724C)
+val Mustard = Color(0xFFFFC529)
+
+val Black01 = Color(0x00494D63)
+val Black10 = Color(0xFF191B2F)
+val Black12 = Color(0xFF30384F)
