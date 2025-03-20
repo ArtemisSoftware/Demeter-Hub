fc0aca13dfc2fd365af9504bde65fbcd0046177c
gustavo <gustavo.isaac@gmail.com>
2025-03-20 18:22:37 +0000

    AuthenticationNavGraph

---
+++ app/src/main/java/com/artemissoftware/demeterhub/feature/authentication/navigation/AuthenticationNavGraph.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.demeterhub.feature.authentication.navigation
+
+import android.annotation.SuppressLint
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.artemissoftware.demeterhub.feature.authentication.welcome.WelcomeScreen
+
+
+@SuppressLint("ComposableDestinationInComposeScope")
+fun NavGraphBuilder.authenticationNavGraph(
+    navController: NavHostController,
+) {
+
+    composable<AuthenticationRoute.Welcome> {
+        WelcomeScreen(
+            navigateToLogin = {},
+            navigateToSignUp = {}
+        )
+    }
+}
