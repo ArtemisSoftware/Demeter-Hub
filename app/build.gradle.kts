2ade32e71cc6ec2b28f0c61d343398674791cdac
gustavo <gustavo.isaac@gmail.com>
2025-03-20 18:19:36 +0000

    Kotlin serialization dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -61,6 +62,9 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.moshi)
     implementation(libs.logging.interceptor)
+
+    //implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 
 
     testImplementation(libs.junit)
