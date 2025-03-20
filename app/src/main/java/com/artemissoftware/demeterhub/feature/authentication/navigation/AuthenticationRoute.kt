90fd773a1da52344a8521a9d291badd171e7a257
gustavo <gustavo.isaac@gmail.com>
2025-03-20 18:19:44 +0000

    AuthenticationRoute

---
+++ app/src/main/java/com/artemissoftware/demeterhub/feature/authentication/navigation/AuthenticationRoute.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.demeterhub.feature.authentication.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class AuthenticationRoute {
+    @Serializable
+    object Welcome
+}
