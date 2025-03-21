69ca655fde47aaa8eeeaa1c495950eafbf5e003d
gustavo <gustavo.isaac@gmail.com>
2025-03-21 19:22:30 +0000

    DataError

---
+++ app/src/main/java/com/artemissoftware/demeterhub/core/domain/error/DataError.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.demeterhub.core.domain.error
+
+sealed interface DataError : Error {
+
+    sealed class NetworkError : DataError {
+        data class Error(val message: String) : NetworkError()
+        data object SocketTimeout : NetworkError()
+        data object Unknown : NetworkError()
+        data object UnknownHost : NetworkError()
+        data object Connect : NetworkError()
+    }
+}
