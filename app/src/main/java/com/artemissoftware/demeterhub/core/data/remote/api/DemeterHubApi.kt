aa408566085b86c9cc3ade3febcc62aba2144e20
gustavo <gustavo.isaac@gmail.com>
2025-03-22 15:01:56 +0000

    DemeterHubApi

---
+++ app/src/main/java/com/artemissoftware/demeterhub/core/data/remote/api/DemeterHubApi.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.demeterhub.core.data.remote.api
+
+import com.artemissoftware.demeterhub.core.data.remote.dto.authentication.AuthenticationDto
+import com.artemissoftware.demeterhub.core.data.remote.dto.authentication.SignUpRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface DemeterHubApi {
+
+    @POST("/auth/signup")
+    suspend fun signUp(@Body request: SignUpRequest): AuthenticationDto
+}
