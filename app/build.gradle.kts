ff5e5e46f1f91988f036190f8ed441efe815696c
gustavo <gustavo.isaac@gmail.com>
2025-03-22 19:57:11 +0000

    Datastore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.converter.moshi)
     implementation(libs.logging.interceptor)
 
+    implementation(libs.datastore)
+
     implementation(libs.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
 
