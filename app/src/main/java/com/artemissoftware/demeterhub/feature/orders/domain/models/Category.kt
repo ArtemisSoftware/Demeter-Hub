3f450c7c7c666dc499e55eadc897f3c66b069c83
gustavo <gustavo.isaac@gmail.com>
2025-04-01 13:56:06 +0100

    Category

---
+++ app/src/main/java/com/artemissoftware/demeterhub/feature/orders/domain/models/Category.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.demeterhub.feature.orders.domain.models
+
+data class Category(
+    val createdAt: String,
+    val id: String,
+    val imageUrl: String,
+    val name: String
+)
